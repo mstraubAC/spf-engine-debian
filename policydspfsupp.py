1a961158d5c54567536337aaaa89565af9c0f6af
SVN-Git Migration <python-apps-team@lists.alioth.debian.org>
2018-02-24 08:56:51 -0800

    Imported Upstream version 0.7.2

--- policydspfsupp.py
+++ policydspfsupp.py
@@ -18,7 +18,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.'''
 
-import syslog, os, sys, string, re, time, popen2, urllib, stat
+import syslog, os, sys, string, re, time, urllib, stat
 
 
 #  default values
